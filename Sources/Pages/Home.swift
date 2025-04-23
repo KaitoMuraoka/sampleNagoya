e88349b77218c3e6a1814dbccccfad7bcbb38c3e
KaitoMuraoka <kaitoboshi@icloud.com>
2025-04-24 00:06:35 +0900

    second commit

--- Sources/Pages/Home.swift
+++ Sources/Pages/Home.swift
@@ -7,5 +7,11 @@ struct Home: StaticPage {
     var body: some HTML {
         Text("Hello world!")
             .font(.title1)
+        
+        Text("Try! Swift Tokyo 2025")
+        Embed(youTubeID: "https://youtu.be/eGNKJaj1rC8?si=Trtld41Xt6ccz_Mi", title: "8番出口")
+        Embed(youTubeID: "https://youtu.be/WOlW7PonJFE?si=cgDr7p62IJTjAyke", title: "Japan Symbol Quize")
+        
+        Embed(spotifyID: "1KLg01cjnRsENoFhJWUTSd?utm_source=generator", title: "Plazma", type: .album)
     }
 }
